61c2c1b882a4580a03088047403b97860b8532b8
arshali2774 <arshali.763z@gmail.com>
2023-08-10 18:11:29 +0530

    navbar styled

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,46 @@
 import React from 'react';
 import Wrapper from '../assets/wrappers/Navbar';
 import { FaAlignLeft, FaUserCircle, FaCaretDown } from 'react-icons/fa';
+import Logo from './Logo';
+import { useDispatch, useSelector } from 'react-redux';
 const Navbar = () => {
+  const { user } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
   return (
     <Wrapper>
-      <h2>Navbar</h2>
-      <FaAlignLeft />
-      <FaCaretDown />
-      <FaUserCircle />
+      <div className='nav-center'>
+        <button
+          type='button'
+          className='toggle-btn'
+          onClick={() => console.log('toggle sidebar')}
+        >
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h3 className='logo-text'>dashboard</h3>
+        </div>
+        <div className='btn-container'>
+          <button
+            type='button'
+            className='btn'
+            onClick={() => console.log('Dropdown toggle')}
+          >
+            <FaUserCircle />
+            {user.name}
+            <FaCaretDown />
+          </button>
+          <div className='dropdown show-dropdown'>
+            <button
+              type='button'
+              className='dropdown-btn'
+              onClick={() => console.log('logout')}
+            >
+              Logout
+            </button>
+          </div>
+        </div>
+      </div>
     </Wrapper>
   );
 };
