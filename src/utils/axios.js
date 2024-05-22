6b74f08ed8386b06303901ff6ef8a3f6ca14232b
arshali2774 <arshali.763z@gmail.com>
2024-05-22 17:19:37 +0530

    added www

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getUserFromLocalStorage } from './localStorage';
 
 const customFetch = axios.create({
-  baseURL: 'https://jobify-prod.herokuapp.com/api/v1/toolkit',
+  baseURL: 'https://www.jobify-prod.herokuapp.com/api/v1/toolkit',
 });
 
 customFetch.interceptors.request.use(
